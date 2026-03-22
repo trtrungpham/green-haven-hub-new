204f1d80647b5fe50112bb6ddd71c0356fc25dac
User <user@example.com>
2026-03-22 15:22:58 +0700

    Fix base path for assets on Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: "./",
+  base: "/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
